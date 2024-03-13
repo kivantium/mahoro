243fe031bd68cb16b17b0ee0a81a3db37571f6c2
amane-lyric <amane.lyric@gmail.com>
2024-03-13 14:56:50 +0900

    use api

--- kabu.py
+++ kabu.py
@@ -43,13 +43,20 @@ headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 if isOpen(today) and d.hour >= 10 and d.hour <= 15:
     #日経平均
     try:
-        res = requests.get('https://finance.yahoo.com/quote/%5EN225?p=%5EN225', headers=headers)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        nikkei_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)','data-symbol':'^N225'}).string
-        nikkei_change = soup.find('fin-streamer', {'class':'Fw(500) Pstart(8px) Fz(24px)','data-symbol':'^N225'}).string
-        message += "日経平均: {price}円 (前日比{change}円)\n".format(price=(nikkei_price), change=str(nikkei_change))
+        res = requests.get("https://query2.finance.yahoo.com/v8/finance/chart/%5EN225?range=1m", headers=headers, timeout=5).json()
+        nikkei_price = float(res['chart']['result'][0]['meta']['regularMarketPrice'])
+        nikkei_previousClose = float(res['chart']['result'][0]['meta']['previousClose'])
+        nikkei_change = nikkei_price - nikkei_previousClose
+        message += "日経平均: {price}円 (前日比{sign}{change:.2f}円)\n".format(price=str(nikkei_price), sign=("+" if nikkei_change > 0 else ""), change=nikkei_change)
     except:
-        pass
+        try:
+            res = requests.get('https://finance.yahoo.com/quote/%5EN225?p=%5EN225', headers=headers, timeout=5)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            nikkei_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)','data-symbol':'^N225'}).string
+            nikkei_change = soup.find('fin-streamer', {'class':'Fw(500) Pstart(8px) Fz(24px)','data-symbol':'^N225'}).string
+            message += "日経平均: {price}円 (前日比{change}円)\n".format(price=(nikkei_price), change=str(nikkei_change))
+        except:
+            pass
 
     #TOPIX
     #rest = requests.get('http://stocks.finance.yahoo.co.jp/stocks/detail/?code=998405')
@@ -62,31 +69,44 @@ us_holidays = holidays.UnitedStates()
 
 if ustime.date().weekday() <= 4 and ustime.date() not in us_holidays and ustime.hour >= 10 and ustime.hour <= 16:
     try:
-        res = requests.get('https://finance.yahoo.com/quote/%5EDJI?p=%5EDJI', headers=headers)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        dow_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)','data-symbol':'^DJI'}).string
-        dow_change = soup.find('fin-streamer', {'class':'Fw(500) Pstart(8px) Fz(24px)','data-symbol':'^DJI'}).string
-        message += "ダウ平均: {price}ドル (前日比{change}ドル)\n".format(price=(dow_price), change=str(dow_change))
-        #spc_price = soup.find('span', {'data-reactid':'303'}).get_text()
-        #spc_change = soup.find('span', {'data-reactid':'305'}).get_text()
-        #message += "S&P 500: {price} (前日比{change})\n".format(price=(spc_price), change=str(spc_change))
+        res = requests.get("https://query2.finance.yahoo.com/v8/finance/chart/%5EDJI?range=1m", headers=headers, timeout=5).json()
+        dow_price = float(res['chart']['result'][0]['meta']['regularMarketPrice'])
+        dow_previousClose = float(res['chart']['result'][0]['meta']['previousClose'])
+        dow_change = dow_price - dow_previousClose
+        message += "ダウ平均: {price}ドル (前日比{sign}{change:.2f}ドル)\n".format(price=str(dow_price), sign=("+" if dow_change > 0 else ""), change=dow_change)
     except:
-        pass
+        try:
+            res = requests.get('https://finance.yahoo.com/quote/%5EDJI?p=%5EDJI', headers=headers, timeout=5)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            dow_price = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)','data-symbol':'^DJI'}).string
+            dow_change = soup.find('fin-streamer', {'class':'Fw(500) Pstart(8px) Fz(24px)','data-symbol':'^DJI'}).string
+            message += "ダウ平均: {price}ドル (前日比{change}ドル)\n".format(price=(dow_price), change=str(dow_change))
+            #spc_price = soup.find('span', {'data-reactid':'303'}).get_text()
+            #spc_change = soup.find('span', {'data-reactid':'305'}).get_text()
+            #message += "S&P 500: {price} (前日比{change})\n".format(price=(spc_price), change=str(spc_change))
+        except:
+            pass
+
 
 try:
-    res = requests.get('https://finance.yahoo.com/quote/JPY%3DX?p=JPY%3DX', headers=headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    dollar = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).string
+    res = requests.get("https://query2.finance.yahoo.com/v8/finance/chart/JPY%3DX?p=JPY%3DX'?range=1m", headers=headers, timeout=5).json()
+    dollar = res['chart']['result'][0]['meta']['regularMarketPrice']
     message += "1ドル: {:.2f}円\n".format(float(dollar))
 except:
     try:
-        res = requests.get('https://finance.yahoo.com/currencies', headers=headers)
+        res = requests.get('https://finance.yahoo.com/quote/JPY%3DX?p=JPY%3DX', headers=headers, timeout=5)
         soup = BeautifulSoup(res.text, 'html.parser')
-        search = re.compile('.*USD/JPY.*')
-        dollar = soup.find(text=search).find_next().text
+        dollar = soup.find('fin-streamer', {'class':'Fw(b) Fz(36px) Mb(-4px) D(ib)'}).string
         message += "1ドル: {:.2f}円\n".format(float(dollar))
     except:
-        pass
+        try:
+            res = requests.get('https://finance.yahoo.com/currencies', headers=headers, timeout=5)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            search = re.compile('.*USD/JPY.*')
+            dollar = soup.find(text=search).find_next().text
+            message += "1ドル: {:.2f}円\n".format(float(dollar))
+        except:
+            pass
 
 btc = requests.get('https://api.bitflyer.jp/v1/ticker?product_code=BTC_JPY').json()['ltp']
 message += "現在のビットコイン価格は{price}円です".format(price=str(btc))
